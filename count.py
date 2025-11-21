ec610add61f783f82c9f0fa920afc45e3de89668
Leo <leonidivanovzalgo@gmail.com>
2025-11-21 19:25:46 +0100

    fixed count

--- count.py
+++ count.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
-FOLDER = "dataset-28"
+SMALL = "dataset-28"
+BIG = "dataset-64"
 
-count = sum(1 for f in Path(FOLDER).rglob("*") if f.is_file())
-print(count)
+def get_count(folder):
+    return sum(1 for f in Path(folder).rglob("*") if f.is_file())
+
+print(f"images in the 28x28 dataset: {get_count(SMALL)}\nimages in the 64x64 dataset: {get_count(BIG)}")
